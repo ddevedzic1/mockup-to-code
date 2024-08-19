2da15fbc4a59fdd90145e4cbe7e479eaa3e4c542
ddevedzic1 <ddevedzic1@etf.unsa.ba>
2024-08-19 23:15:39 +0200

    Configured React Router

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,8 +1,12 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
