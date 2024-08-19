aa4138d7c5e1acaee47b149d5e184ef21a690273
ddevedzic1 <ddevedzic1@etf.unsa.ba>
2024-08-20 00:29:20 +0200

    Configured Redux Toolkit

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -1,14 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
+import { Provider } from "react-redux";
 
 import App from './App.jsx'
 import theme from "../../theme.js";
+import store from "./store";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ChakraProvider theme={theme}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ChakraProvider>
   </StrictMode>,
 )
