2fc18e4fab0d772b8a389d9eee68d6886d548222
ddevedzic1 <ddevedzic1@etf.unsa.ba>
2024-09-06 21:12:14 +0200

    Implemented html code generator

---
+++ src/treeModel/codeGenerators/generateHtml.js
@@ -0,0 +1,33 @@
+export const generateHtml = tree => {
+  const htmlBody = generateHtmlBody(tree);
+  return `<!DOCTYPE html>
+  <html>
+  <head>
+  <title>Page title</title>
+  <link href="./index.css" rel="stylesheet" />
+  </head>
+  <body>${htmlBody}
+  <script defer src="./index.js" />
+  </body>
+  </html>`;
+};
+
+const generateHtmlBody = tree => {
+  if (typeof tree === 'string') return tree;
+
+  const { id, tag, children = [], attributes = {} } = tree;
+  const attributesString = generateAttributesString(id, attributes);
+
+  const generatedChildren = children.map(generateHtmlBody).join('');
+
+  return `\n<div>\n<${tag} id="${id}" ${attributesString}>${generatedChildren}</${tag}>\n</div>`;
+};
+
+const generateAttributesString = (id, attributes) => {
+  return Object.keys(attributes).reduce((acc, attr) => {
+    if (attr === 'style') return acc;
+    let attrValue = attributes[attr];
+    if (attr === 'onclick') attrValue = `${id}()`;
+    return acc + `${attr}="${attrValue}" `;
+  }, '');
+};
