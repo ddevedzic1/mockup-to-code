585f55a0df66aa93b0150d46a256eb82966b1fed
ddevedzic1 <ddevedzic1@etf.unsa.ba>
2024-09-06 21:12:26 +0200

    Implemented css generator

---
+++ src/treeModel/codeGenerators/generateCss.js
@@ -0,0 +1,30 @@
+import { camelToKebab } from '../../utils/helpers';
+
+export const generateCss = tree => {
+  const generatedCss = generateCssOfAddedElements(tree);
+  return `html, body {\n    margin: 0\n}\n\n${generatedCss}`;
+};
+
+const generateCssOfAddedElements = tree => {
+  let generatedCss = '';
+  const { id, children = [], attributes = {} } = tree;
+  const { style = {} } = attributes;
+  if (Object.keys(style).length > 0) {
+    const properties = generateProperties(style);
+    const currentElementCss = `#${id} {\n${properties}}\n\n`;
+    generatedCss += currentElementCss;
+  }
+  return children.reduce((acc, child) => {
+    const newCss = generateCssOfAddedElements(child);
+    if (newCss) return acc + newCss;
+    return acc;
+  }, generatedCss);
+};
+
+const generateProperties = style => {
+  return Object.keys(style).reduce(
+    (acc, property) =>
+      acc + `    ${camelToKebab(property)}: ${style[property]};\n`,
+    ''
+  );
+};
