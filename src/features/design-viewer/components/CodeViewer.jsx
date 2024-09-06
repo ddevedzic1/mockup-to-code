549977f0413b9e4c12dfce5154724d5cbfa3cd43
ddevedzic1 <ddevedzic1@etf.unsa.ba>
2024-09-06 22:07:24 +0200

    Added copy button

--- src/features/design-viewer/components/CodeViewer.jsx
+++ src/features/design-viewer/components/CodeViewer.jsx
@@ -1,5 +1,6 @@
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react"
+import { Tabs, TabList, TabPanels, Tab, TabPanel, Button, useToast, Spacer } from "@chakra-ui/react"
 import { useSelector } from "react-redux"
+import { useState } from 'react';
 
 import CodeBlock from "../../../components/CodeBlock"
 import { generateHtml } from "../../../treeModel/codeGenerators/generateHtml"
@@ -12,12 +13,40 @@ function CodeViewer() {
     const generatedHtml = generateHtml(tree);
     const generatedCss = generateCss(tree);
     const generatedJavaScript = generateJavaScript(tree);
+
+    const toast = useToast();
+    const [activeTab, setActiveTab] = useState(0);
+    const [activeTabContent, setActiveTabContent] = useState(generatedHtml);
+
+    const handleTabChange = (index) => {
+        setActiveTab(index);
+        if (index === 0) setActiveTabContent(generatedHtml);
+        if (index === 1) setActiveTabContent(generatedCss);
+        if (index === 2) setActiveTabContent(generatedJavaScript)
+    };
+
     return (
-        <Tabs variant="unstyled">
+        <Tabs variant="unstyled" index={activeTab} onChange={handleTabChange}>
             <TabList>
                 <Tab>HTML</Tab>
                 <Tab>CSS</Tab>
                 <Tab>JavaScript</Tab>
+                <Spacer />
+                <Button
+                    bgColor="gray.800"
+                    onClick={() => {
+                        navigator.clipboard.writeText(activeTabContent);
+                        toast({
+                            title: "Copied",
+                            position: "bottom-right",
+                            status: "info",
+                            duration: 1000,
+                            isClosable: true,
+                        });
+                    }}
+                >
+                    Copy
+                </Button>
             </TabList>
             <TabPanels>
                 <TabPanel>
