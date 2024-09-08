2e34e417072469491ca745610fe4d864eec20430
ddevedzic1 <ddevedzic1@etf.unsa.ba>
2024-09-08 14:00:08 +0200

    Removed design canvas component

--- src/features/design-viewer/DesignViewer.jsx
+++ src/features/design-viewer/DesignViewer.jsx
@@ -1,8 +1,26 @@
-import DesignCanvas from "./components/DesignCanvas"
+import { Box } from "@chakra-ui/react"
+import { Resizable } from 're-resizable';
+
+import TreeAsReact from "./components/TreeAsReact"
+import useDroppableMonitor from "./hooks/useDroppableMonitor";
 
 function DesignViewer() {
+    useDroppableMonitor();
+    const style = { margin: "0", padding: "0", top: "0", left: "50%", position: "absolute", transform: "translate(-50%)", height: "100%", backgroundColor: "white" }
     return (
-        <DesignCanvas />
+        <Resizable
+            defaultSize={{
+                width: "90%",
+                height: "100%"
+            }}
+            style={style}
+            maxWidth="100%"
+            maxHeight="100%"
+        >
+            <Box style={{ all: "initial" }}>
+                {<TreeAsReact />}
+            </Box>
+        </Resizable>
     )
 }
 
